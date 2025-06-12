a2a8884216a138e93a70f221388d391b3e09e17b
Ricardo00899 <00000725@uca.edu.sv>
2025-06-12 10:37:14 -0600

    Base de el proyecto y idea

---
+++ proyecto/proyecto.cpp
@@ -0,0 +1,17 @@
+/*Desarrollaremos un juego, llamado la mesa de los mentirosos, este juego puede ser de 2-4 jugadores los cuales se les 
+repartiran 5 cartas a cada uno de ellos, en el mazo pueden haber reyes reinas, As y un solo joacker, repartidas las cartas
+de manera aleatoria, la mesa (o un jugador siguiendo un orden), reclamaran a la mesa una carta (sea rey, reina, As o joacker), y todos tendran
+que entregar cartas sea 1-2-3, pero no 4 ni 5, una vez tiradas los jugadores podran decidir si creerle a el jugador o dudar y declararlo
+MENTIROSO, si el que tiro tiene cartas de las que se reclamaron, el reclamador queda como emntiroso y pierde, asi de manera contraria
+si el tirador no tiene cartas de las que se reclamo y tiro, queda como mentiroso y pierde, luego se regresaran las cartas se volveran a 
+barajear y se repartiran, sin el jugador que perdio esta vez, este teniendo que cumplir un castigo o reto. */
+#include <iostream>
+using namespace std;
+
+int main (){
+    
+
+
+
+    return 0;
+}
