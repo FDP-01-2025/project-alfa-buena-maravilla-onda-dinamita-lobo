28d57af79c5cdee40106bb2a3c6664a654c051be
GaelAlas <00033625@uca.edu.sv>
2025-06-29 22:42:19 -0600

    Creacion de archivos .h

---
+++ src/header.h
@@ -0,0 +1,15 @@
+                        //Declaracion de librerias
+#include <iostream>
+//ya usada en clase que nos ayudara con cadenas de texto 
+#include <string>
+//esta libreria nos ayudara con la generacion de numeros ramdom o utilidades ramdomizando ciertas cosas.
+#include <cstdlib>
+//esta libreria es en funcion al tiempo, utilizda para poder inicializar el barajeo ramdoom, la idea es que haga que
+//cada barajeo sea diferente
+#include <ctime>
+                        //Fin declaracion de librerias
+using namespace std;
+
+//Declaracion de funciones
+
+//Fin declaracion de funciones
